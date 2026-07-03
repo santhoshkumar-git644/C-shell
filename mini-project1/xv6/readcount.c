18b48967e6f163899a5ed7600c586619b92e7d2c
santhosh <santhosh2007-git>
2026-07-03 11:13:32 +0530

    Move xv6 files to correct directory

--- mini-project1/xv6/readcount.c
+++ mini-project1/xv6/readcount.c
@@ -1,1 +1,18 @@
-int getreadcount() { return 0; }
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fcntl.h"
+
+int main() {
+    int initial = getreadcount();
+    printf("Initial readcount: %d\n", initial);
+    
+    int fd = open("README", O_RDONLY);
+    char buf[100];
+    read(fd, buf, 100);
+    close(fd);
+    
+    int final = getreadcount();
+    printf("Final readcount: %d\n", final);
+    exit(0);
+}
